ccc502beeea728fa21a7b8fd254a7fa637f2bdaa
Opdata <pb03641@gmail.com>
2021-02-02 16:10:48 +0900

    Basic Tag Add, Actions Add

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux';
+import { add, complete } from './reducers/todoList';
+import { input, submit, status } from './reducers/todoItem';
+
+/**
+ * todoItem
+ * @property input
+ * @property submit
+ * @property status
+ */
+
+/**
+ * todoList
+ * @property add
+ * @property complete
+ */
 
 function App() {
+  // onclick, onsubmit
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input></input>
+        <button>추가</button>
+      </form>
     </div>
   );
 }
