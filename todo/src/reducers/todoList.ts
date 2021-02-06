1f5c2f7d7f6b4671e35d2cadedb1aba38d02dc55
Opdata <pb03641@gmail.com>
2021-02-06 17:30:37 +0900

    RootState interface import, add action edit

--- todo/src/reducers/todoList.ts
+++ todo/src/reducers/todoList.ts
@@ -1,23 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from './todoItem';
 
-const initialState: any = [];
+const initialState: Array<RootState | undefined> = [];
 
 const todoList = createSlice({
   name: 'todoList',
   initialState,
   reducers: {
-    add: (state, payload) => {
-      state.push(payload);
+    add: (state, action) => {
+      state.push(action.payload);
     },
 
     complete: (state) => {
-      console.log(state);
+      // console.log(state);
     },
   },
 });
 
 export const { add, complete } = todoList.actions;
 
-export const selectedList = (state: any) => state.todoList;
+export const selectList = (state: any) => state.todoList;
 
 export default todoList.reducer;
